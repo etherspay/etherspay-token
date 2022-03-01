59bb0f983e8f806f2534386b73b31cc58cc4bfc8
Ray Orolé <orole.ray@protonmail.com>
2022-03-01 22:46:13 +0100

    Improved tests

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,55 +8,100 @@ contract ERC20 is IERC20 {
     string public name = "etherspay";
     string public symbol = "EPT";
     uint8 public decimals = 18;
-    uint256 public _totalSupply = 201020210 ether; // 201 million tokens
+    uint256 public _totalSupply = 21000000 ether; // 21 million tokens
 
     using SafeMath for uint256;
 
-    mapping(address => uint256) balances;
-    mapping(address => mapping (address => uint256)) allowed;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowed;
 
     constructor() {
-        balances[msg.sender] = _totalSupply;
+        _balances[msg.sender] = _totalSupply;
     }
 
-    function totalSupply() public override view returns (uint256) {
+    function totalSupply() public view override returns (uint256) {
         return _totalSupply;
     }
 
-    function balanceOf(address tokenOwner) public override view returns (uint256) {
-        return balances[tokenOwner];
+    function balanceOf(address tokenOwner) public view override returns (uint256) {
+        return _balances[tokenOwner];
     }
 
-    function transfer(address receiver, uint256 amount) public override returns (bool) {
-        require(amount <= balances[msg.sender]);
-        
-        balances[msg.sender] = balances[msg.sender].sub(amount);
-        balances[receiver] = balances[receiver].add(amount);
-        
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowed[owner][spender];
+    }
+
+    function transfer(address receiver, uint256 amount) public override returns (bool)
+    {
+        require(amount <= _balances[msg.sender]);
+        require(receiver != address(0), 'Receiver address cannot be zero address');
+
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        _balances[receiver] = _balances[receiver].add(amount);
+
         emit Transfer(msg.sender, receiver, amount);
         return true;
     }
 
     function approve(address spender, uint256 amount) public override returns (bool) {
-        allowed[msg.sender][spender] = amount;
+        _allowed[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    function allowance(address owner, address spender) public override view returns (uint) {
-        return allowed[owner][spender];
-    }
 
-    function transferFrom(address owner, address buyer, uint256 amount) public override returns (bool) {
-        require(amount <= balances[owner]);
-        require(amount <= allowed[owner][msg.sender]);
+    function transferFrom(address owner, address buyer, uint256 amount ) public override returns (bool) {
+        require(amount <= _balances[owner], 'Insufficient balance');
+        require(amount <= _allowed[owner][msg.sender], 'Insufficient allowance');
 
-        balances[owner] = balances[owner].sub(amount);
-        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(amount);
-        
-        balances[buyer] = balances[buyer].add(amount);
+        _balances[owner] = _balances[owner].sub(amount);
+        _allowed[owner][msg.sender] = _allowed[owner][msg.sender].sub(amount);
+
+        _balances[buyer] = _balances[buyer].add(amount);
         emit Transfer(owner, buyer, amount);
         return true;
     }
 
+    function increaseAllowance( address spender, uint256 addedValue) public returns (bool) {
+        require(spender != address(0));
+
+        _allowed[msg.sender][spender] = (
+        _allowed[msg.sender][spender].add(addedValue));
+        emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        require(spender != address(0));
+
+        _allowed[msg.sender][spender] = (
+        _allowed[msg.sender][spender].sub(subtractedValue));
+        emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+        return true;
+    }
+
+
+    function mint(address account, uint256 amount) public {
+        require(account != address(0));
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+
+    function burn(address account, uint256 amount) public {
+        require(account != address(0));
+        require(amount <= _balances[account]);
+
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[account] = _balances[account].sub(amount);
+        emit Transfer(account, address(0), amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public {
+        require(amount <= _allowed[account][msg.sender]);
+
+        _allowed[account][msg.sender] = _allowed[account][msg.sender].sub(amount);
+        burn(account, amount);
+    }
 }
--- test/main-test.js
+++ test/main-test.js
@@ -1,12 +1,56 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { utils } = require("ethers");
+const { zeroAddress } = require("ethereumjs-util");
+
+const TEST_AMOUNT = 1000;
 
 describe("Deployment", function () {
-  it("Should return the correct token symbol", async function () {
-    const contract = await ethers.getContractFactory("ERC20");
-    const erc20 = await contract.deploy();
-    await erc20.deployed();
+  beforeEach(async () => {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    contract = await ethers.getContractFactory("ERC20");
+    token = await contract.deploy();
+  });
 
-    expect(await erc20.symbol()).to.equal("EPT");
+  it("Should return the correct token name, symbol, supply", async function () {
+    expect(await token.name()).to.equal("etherspay");
+    expect(await token.symbol()).to.equal("EPT");
+
+    const totalSupply = await token.totalSupply();
+    const totalSupplyEther = Math.floor(utils.formatEther(totalSupply));
+    expect(totalSupplyEther).to.equal(21000000);
+  });
+
+  it("Should return the correct balance off contract owner when deployed", async function () {
+    // Expect balance of contract owner to equal 21 million
+    expect(
+      Math.floor(utils.formatEther(await token.balanceOf(owner.address)))
+    ).to.equal(21000000);
+  });
+
+  it("approve", async () => {
+    await expect(token.approve(addr1.address, TEST_AMOUNT))
+      .to.emit(token, "Approval")
+      .withArgs(owner.address, addr1.address, TEST_AMOUNT);
+
+    expect(await token.allowance(owner.address, addr1.address)).to.equal(
+      TEST_AMOUNT
+    );
+  });
+
+  it("transfer", async () => {
+    await expect(token.transfer(addr1.address, TEST_AMOUNT))
+      .to.emit(token, "Transfer")
+      .withArgs(owner.address, addr1.address, TEST_AMOUNT);
+    expect(
+      Math.floor(utils.formatEther(await token.balanceOf(owner.address)))
+    ).to.eq(21000000);
+    expect(await token.balanceOf(addr1.address)).to.eq(TEST_AMOUNT);
+  });
+
+  it("burn", async () => {
+    await expect(token.burn(owner.address, TEST_AMOUNT))
+      .to.emit(token, "Transfer")
+      .withArgs(owner.address, zeroAddress, TEST_AMOUNT);
   });
 });
