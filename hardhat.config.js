53c8fb15bd10efcdcdee4054dad2f7af293b95d5
Ray Orolé <orole.ray@protonmail.com>
2022-07-21 01:29:55 +0200

    Add truffle network

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,4 +13,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    truffle: {
+      url: "http://localhost:7545",
+      accounts: ["458327ea710da0f65168481a6e956815eb4427019dd976b4ef0b3948f6ce8672"]
+    } 
+  }
 };
